135ab118e0f54c40757bd320544dcaacc1e7064f
LAPTOP-RGM9IGTS\lupit <lupito_guerrero@hotmail.com>
2018-10-29 20:56:42 -0600

    busqueda, edición y eliminación listo

--- Tareas/Proyecto Final/ProyectoF.cpp
+++ Tareas/Proyecto Final/ProyectoF.cpp
@@ -112,7 +112,6 @@ void registrar() {
 			cout << "telefono incorrecto, vuelva a registrar los datos\n";
 		}
 	}
-	int contador = 0;
 	cout << "Matricula ¿generar(0) o ingresar(1)? ";
 	int opm;
 	cin >> opm;
@@ -195,33 +194,162 @@ void mod() {
 		}
 	}
 	if (!encontrado) {
-		cout << "no se encontró, caballero :(";
-	}
-	cout << "\n¿Que desea hacer con el usuario\n" << P[elegido].matricula << "?";
-	cout << "1.- Modificar \n 2.-Eliminar\n";
-	cin >> opmod;
-	while (opmod != 1 && opmod != 2) {
-		cout << "no valido, vuelva a seleccionar: ";
+		cout << "no se encontró, caballero :(" << endl;
+		cout << "volver a intentar(1) o regresar(2): ";
 		cin >> opmod;
+		while (opmod != 1 && opmod != 2) {
+			cout << "no valido, vuelva a seleccionar: ";
+			cin >> opmod;
+		}
+		switch (opmod)
+		{
+		case 1: mod();
+			break;
+		case 2: menu();
+			break;
+		}
 	}
-	switch (opmod)
-	{
-	case 1: edit(elegido);
-		break;
-	case 2: delet(elegido);
-		break;
+	else {
+		cout << "\n¿Que desea hacer con el usuario\n" << P[elegido].matricula << "?" << endl;
+		cout << "1.- Modificar \n 2.-Eliminar\n";
+		cin >> opmod;
+		while (opmod != 1 && opmod != 2) {
+			cout << "no valido, vuelva a seleccionar: ";
+			cin >> opmod;
+		}
+		switch (opmod)
+		{
+		case 1: edit(elegido);
+			break;
+		case 2: delet(elegido);
+			break;
+		}
 	}
 }
 
 void edit(int x) {
 	system("cls");
 	cout << "¿que desea editar del usuario " << P[x].matricula << "?";
-	cout << "\n1.-Nombre \n2.-Apellido \n3.-Correo \n4.-Teléfono \n5.-Dirección \n6.-primer parcial \n7.-segundo parcial \n8.-tercer parcial";
-	int opedit;
+	cout << "\n1.-Nombre \n2.-Apellido \n3.-Correo \n4.-Teléfono \n5.-Calle \n6.-Colonia \n7.-primer parcial \n8.-segundo parcial \n9.-tercer parcial\n";
+	int opedit, x2;
+	bool califnueva = false, condition1, condition2, condition3;
 	cin >> opedit;
+	switch (opedit) {
+	case 1: cout << "Inserte el nuevo nombre: ";
+		cin.ignore();
+		getline(cin, P[x].nombre);
+		break;
+	case 2: cout << "Inserte nuevo apellido: ";
+		cin.ignore();
+		getline(cin, P[x].apellido);
+		break;
+	case 3: cout << "Inserte nuevo correo electrónico: ";
+		condition1 = false, condition2 = false;
+		int i;
+		x2 = 0;
+		while (!condition1 || !condition2) {
+			cin >> P[x].correo;
+			for (i = 0; i < 30; i++) {
+				if (P[x].correo[i] == 64) {
+					condition1 = true;
+					break;
+					x2 = i;
+				}
+			}
+			for (i = x2; i < 30; i++) {
+				if (P[x].correo[i] == 46) {
+					if (P[x].correo[i + 1] == 99) {
+						if (P[x].correo[i + 2] == 111) {
+							if (P[x].correo[i + 3] == 109) {
+								condition2 = true;
+								break;
+							}
+						}
+					}
+				}
+			}
+			if (!condition1 || !condition2) {
+				cout << "correo incorrecto, vuelva a registrar los datos\n";
+			}
+		}
+		break;
+	case 4: cout << "Inserte nuevo telefono: ";
+		x2 = 0;
+		condition3 = false;
+		while (!condition3) {
+			cin >> P[x].telefono;
+			for (i = 0; i < 12; i++) {
+				if (P[x].telefono[i] >= 48 && P[x].telefono[i] <= 57) {
+					x2++;
+				}
+			}
+			if (x2 >= 8) {
+				condition3 = true;
+			}
+			if (!condition3) {
+				cout << "telefono incorrecto, vuelva a registrar los datos\n";
+			}
+		}
+		break;
+	case 5: cout << "Ingresar nueva casa y calle: ";
+		cin.ignore();
+		getline(cin, P[x].dircasa);
+		break;
+	case 6: cout << "Ingresar nueva colonia: ";
+		cin.ignore();
+		getline(cin, P[x].dircolon);
+		break;
+	case 7: cout << "Califiación del primer parcial ";
+		cin >> P[x].calif1;
+		while (P[x].calif1 > 100 || P[x].calif1 < 0) {
+			if (P[x].calif1 > 100 || P[x].calif1 < 0) {
+				cout << "\nNo es valido, ponga otra calificación ";
+			}
+			cin >> P[x].calif1;
+		}
+		califnueva = true;
+		break;
+	case 8: cout << "Califiación del segundo parcial ";
+		cin >> P[x].calif2;
+		while (P[x].calif2 > 100 || P[x].calif2 < 0) {
+			if (P[x].calif2 > 100 || P[x].calif2 < 0) {
+				cout << "\nNo es valido, ponga otra calificación ";
+			}
+			cin >> P[x].calif2;
+		}
+		califnueva = true;
+		break;
+	case 9: cout << "Califiación del tercer parcial ";
+		cin >> P[x].calif3;
+		while (P[x].calif3 > 100 || P[x].calif3 < 0) {
+			if (P[x].calif3 > 100 || P[x].calif3 < 0) {
+				cout << "\nNo es valido, ponga otra calificación ";
+			}
+			cin >> P[x].calif3;
+		}
+		califnueva = true;
+		break;
+	}
+	if (califnueva) {
+		P[x].califtotal = (P[x].calif1*0.3) + (P[x].calif2*0.45) + (P[x].calif3*0.25);
+	}
+	cout << "deseas editar algo más (1) o regresar al menú (2): ";
+		cin >> opedit;
+	while (opedit != 1 && opedit != 2) {
+		cout << "no valido, vuelva a seleccionar: ";
+		cin >> opedit;
+	}
+	switch (opedit)
+	{
+	case 1: edit(x);
+		break;
+	case 2: menu();
+		break;
+	}
+
 }
 
-void delet(int x) {
+	void delet(int x) {
 	system("cls");
 	int opdelet;
 	cout << "¿desea eliminar a " << P[x].nombre << ", (" << P[x].matricula << ") ? (1-Si/2-No): ";
